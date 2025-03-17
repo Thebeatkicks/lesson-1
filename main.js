1973a80c7f692ac67ea8f4e4d20c7f19b2b590ff
Thebeatkicks <000henrik@gmail.com>
2025-03-17 12:05:15 +0100

    made the console type out name, age, color

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 console.log("Hello World")
 
+let name; 
+name = "Henrik";
+let age;
+age = 40;
+let color;
+color = "green"; 
+
+console.log(`Hi! my name is ${name} I'm ${age} years old. I love the color ${color}`);
+console.log(typeof name)
+
 let x;
 x = 100;
 let y; 
 y = 50;
-
-console.log(x-y)
